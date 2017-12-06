faa5fddc766bf56f223281a74a031acfb546420f
andrevaETH <vanoncini@hotmail.com>
2017-12-06 13:58:33 +0100

    Add script for experimenting

---
+++ src/main_tests.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Dec 6 2017 1:53 PM
+@author: andrevaETH
+"""
+
+def main():
+    maxLen = 5
+    print("Hello world " + str(maxLen))
+
+if __name__ == '__main__':
+    main()
