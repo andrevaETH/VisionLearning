72de1b605ab70a58c942f8c0d6cf8bc645df8466
andrevaETH <vanoncini@hotmail.com>
2017-12-07 13:40:03 +0100

    Code a replacement for the switch statement

--- src/main_tests.py
+++ src/main_tests.py
@@ -5,13 +5,25 @@ Created on Dec 6 2017 1:53 PM
 """
 
 def main():
-    maxLen = 5
-    print("Hello world " + str(maxLen))
+    maxLen = -1
+    if(maxLen > 5):
+        print("Hello world " + str(maxLen))
+    elif(maxLen < 0):
+        print("bla")
+    else:
+        return 0
 
     i = 0
     while(i < 5):
         print("Sod it")
         i += 1
+
+    # - Create a switch statement replacement -
+    switchDict = {'a' : 1, 'b' : 2}
+
+    keyForDict = 'a'
+    print(switchDict.get(keyForDict, 'default'))
+    
 
 if __name__ == '__main__':
     main()
